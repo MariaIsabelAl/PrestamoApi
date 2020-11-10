a8f80763f223e7d7355db1afafd6f2c3f3364dc8
Maria Isabel Alberto Del Orbe <maria_alberto1@ucne.edu.do>
2020-11-10 12:05:25 -0400

    Add project files.

---
+++ Data/Contexto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using PrestamoAPI.Models;
+
+namespace PrestamoAPI.Data
+{
+    public class Contexto : DbContext
+    {
+        public Contexto(DbContextOptions options) : base(options) { }
+
+        public DbSet<Personas> Personas { get; set; }
+        public DbSet<Prestamos> Prestamos { get; set; }
+    }
+}
